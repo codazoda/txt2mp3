61bc80ca7c906b7e2bcfa99c2950e0d04a74ca8c
Joel Dare <joel@joeldare.com>
2016-09-11 14:26:56 -0600

    updated to separate files

--- txt2mp3.php
+++ txt2mp3.php
@@ -2,7 +2,7 @@
 <?php
 
 /**
- * php2mp3
+ * txt2mp3
  *
  * Reads a text file and converts each line from text to speech adding some silence.
  * The result is a single mp3 audio file with each line separated by that silence.
@@ -13,7 +13,7 @@
  */
 
 // The amount of silence for each recording (1 minute)
-$delay = 60 * 1000;
+$delay = 15 * 1000;
 
 // If a file exists with the name specified
 if (!file_exists($argv[1])) {
@@ -29,23 +29,16 @@ $lines = file($argv[1], FILE_SKIP_EMPTY_LINES | FILE_IGNORE_NEW_LINES);
 
 // Loop through the lines, reading them into aiff files
 foreach ($lines as $number => $line) {
-    $text .= "\"{$line}\"[[slnc {$delay}]] ";
+
+    // Create the aiff file
+    $command = "say -o {$number}.aiff \"{$line}\"[[slnc {$delay}]]";
+    exec($command);
+
+    // Convert the aiff to an mp3
+    $command = "ffmpeg -i {$number}.aiff {$number}.mp3";
+    exec($command);
+
+    // Remove the aiff
+    unlink($number . '.aiff');
+
 }
-
-// If there was a numeric second argument, repeat itself
-if (is_numeric($argv[2])) {
-    for ($i=1; $i<=$argv[2]; $i++) {
-        $text .= $text;
-    }
-}
-
-// Create the aiff file
-$command = "say -o {$filename}.aiff \"{$text}\"[[slnc {$delay}]]";
-exec($command);
-
-// Convert the aiff to an mp3
-$command = "ffmpeg -i {$filename}.aiff {$filename}.mp3";
-exec($command);
-
-// Remove the aiff
-unlink($filename . '.aiff');
